6b99385529d433a567261d9cbd7af6f51db01385
Dijango Alves <dijango_ilha@hotmail.com>
2020-07-03 13:39:07 -0300

    Sign Up implemented.

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -1,23 +1,36 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signUp } from './SignUpActions';
 
 const SignUp = () => {
+
+  const submitHandler = e => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+
+    console.log('*** Sign Up SubmitHandler data  ', data)
+    signUp(data);
+  };
+
   return (
     <div className="container h-100 pt-5">
       <h1>Sign Up</h1>
       <div className="d-flex flex-column h-100">
-        <form>
+        <form onSubmit={submitHandler}>
           <div className="form-group">
             <label>Email</label>
-            <input type="text" className="form-control" placeholder="Enter email" />
+            <input type="text" className="form-control" name="email" placeholder="Enter email" />
           </div>
           <div className="form-group">
             <label>Password</label>
-            <input type="password" className="form-control" placeholder="Password" />
+            <input type="password" className="form-control" name="password" placeholder="Password" />
           </div>
           <div className="form-group">
             <label>Password Confirmation</label>
-            <input type="password" className="form-control" placeholder="Password" />
+            <input type="password" className="form-control" name="password_confirmation" placeholder="Repet Password" />
           </div>
           <div>
             <button type="submit" className="btn btn-primary btn-round">Login</button>
@@ -32,4 +45,8 @@ const SignUp = () => {
   );
 };
 
-export default SignUp;
+const mapStateToProps = (state) => {
+  return { account: state.signUp.account };
+}
+
+export default connect(mapStateToProps, { signUp })(SignUp);
