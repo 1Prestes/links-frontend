1234727e0b5a61e8dafa51aee64689f680bd8390
Dijango Alves <dijango_ilha@hotmail.com>
2020-07-04 13:47:31 -0300

    Update links working.

--- src/screens/Manage/Links/Edit/index.js
+++ src/screens/Manage/Links/Edit/index.js
@@ -3,22 +3,29 @@ import { useParams } from 'react-router-dom';
 import Layout from '../../../Layouts/Manage';
 import FormGroup from '../../../../components/FormGroup';
 import FormCheck from '../../../../components/FormCheck';
-import { linkGet } from '../../../../actions/LinkActions';
+import { linkGet, linkUpdate } from '../../../../actions/LinkActions';
 import { connect } from "react-redux";
+import { getFormData } from "../../../../helpers/form";
 
-const Edit = ({ link, linkGet }) => {
+const Edit = ({ link, linkGet, linkUpdate }) => {
   const { id } = useParams();
 
   useEffect(() => {
     linkGet(id);
   }, [id, linkGet]);
 
+  const submitHandler = e => {
+    e.preventDefault();
+    const data = getFormData(e);
+    return linkUpdate(id, data);
+  }
+
   return (
     <Layout>
       <h1>Edit Link</h1>
       <div>
 
-        <form>
+        <form onSubmit={submitHandler}>
           <FormGroup label="Label" name="label" data={link} type="text" />
           <FormGroup label="Url" name="url" data={link} type="text" />
           <FormCheck label="Is Social" name="isSocial" data={link} />
@@ -39,4 +46,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps, { linkGet })(Edit);
+export default connect(mapStateToProps, { linkGet, linkUpdate })(Edit);
