2d78718c7e650f6e7c1fa896ac9d585284bfb9b6
Dijango Alves Rodolfo <dijango_ilha@hotmail.com>
2020-07-05 00:24:37 -0300

    Implemented refresh token.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
 import store from "./store";
+import TokenRefresher from "./components/TokenRefresher";
 
 import App from "./App";
 import "./styles/main.scss";
 
 ReactDOM.render(
   <Provider store={store}>
+    <TokenRefresher />
     <App />
   </Provider>,
   document.getElementById("root")
--- src/reducers/AccountReducer.js
+++ src/reducers/AccountReducer.js
@@ -3,6 +3,7 @@ import {
   SIGN_UP,
   SIGN_OUT,
   INIT_ACCOUNT,
+  REFRESH_TOKEN,
 } from "../actions/AccountActions";
 import {
   setAccount,
@@ -36,7 +37,6 @@ export default function (state = initialState, action) {
       if (refreshToken) setRefreshToken(refreshToken);
 
       return { ...state, account };
-
     case SIGN_OUT: {
       removeAccount();
       removeToken();
@@ -47,6 +47,13 @@ export default function (state = initialState, action) {
     case INIT_ACCOUNT: {
       const account = getAccount();
       return { ...state, account };
+    }
+    case REFRESH_TOKEN: {
+      const response = payload ? payload.data : null;
+      const metadata = response ? response.metadata : null;
+      const token = metadata ? metadata.token : null;
+      if (token) setToken(token);
+      return state;
     }
     default:
       return state;
